c243beb2afeeb93cbe05b79090042a1504344748
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2022-08-23 18:36:56 +0100

    first commit

---
+++ The Basics of Fission/src/main/java/ml/kineticcat/fission_basics/FissionBasics.java
@@ -0,0 +1,43 @@
+package ml.kineticcat.fission_basics;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+
+@Mod(Utilities.MODID)
+public class FissionBasics {
+    public FissionBasics() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        modEventBus.addListener(this::commonSetup);
+
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = Utilities.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event) {
+        }
+    }
+}
---
+++ The Basics of Fission/src/main/java/ml/kineticcat/fission_basics/Utilities.java
@@ -0,0 +1,11 @@
+package ml.kineticcat.fission_basics;
+
+import com.mojang.logging.LogUtils;
+import org.slf4j.Logger;
+
+public class Utilities {
+
+    public static final String MODID = "fission_basics";
+    public static final Logger LOGGER = LogUtils.getLogger();
+
+}
