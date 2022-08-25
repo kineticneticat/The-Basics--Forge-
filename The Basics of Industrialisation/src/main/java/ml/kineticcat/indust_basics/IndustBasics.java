93185fdd1b15e26f0e3294edc53d287c42fe4a79
kineticneticat <84404038+kineticneticat@users.noreply.github.com>
2022-08-25 18:29:15 +0100

    a

---
+++ The Basics of Industrialisation/src/main/java/ml/kineticcat/indust_basics/IndustBasics.java
@@ -0,0 +1,63 @@
+package ml.kineticcat.indust_basics;
+
+import com.mojang.logging.LogUtils;
+import ml.kineticcat.indust_basics.block.ModBlocks;
+import ml.kineticcat.indust_basics.item.ModItems;
+import net.minecraft.client.Minecraft;
+import net.minecraft.world.item.BlockItem;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.Item;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLCommonSetupEvent;
+import net.minecraftforge.event.server.ServerStartingEvent;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.slf4j.Logger;
+import ml.kineticcat.indust_basics.Utilities;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(Utilities.MODID)
+public class IndustBasics
+{
+    // Directly reference a slf4j logger
+
+
+
+    public IndustBasics() {
+        IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
+
+        // Register the commonSetup method for modloading
+        modEventBus.addListener(this::commonSetup);
+
+        // Register the Deferred Register to the mod event bus so blocks get registered
+        ModBlocks.register(modEventBus);
+        // Register the Deferred Register to the mod event bus so items get registered
+        ModItems.register(modEventBus);
+
+        // Register ourselves for server and other game events we are interested in
+        MinecraftForge.EVENT_BUS.register(this);
+    }
+
+    private void commonSetup(final FMLCommonSetupEvent event) {}
+
+    // You can use SubscribeEvent and let the Event Bus discover methods to call
+    @SubscribeEvent
+    public void onServerStarting(ServerStartingEvent event) {}
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+    @Mod.EventBusSubscriber(modid = Utilities.MODID, bus = Mod.EventBusSubscriber.Bus.MOD)
+    public static class ClientModEvents {
+        @SubscribeEvent
+        public static void onClientSetup(FMLClientSetupEvent event){}
+    }
+}
